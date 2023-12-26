15cd11aa64c297204556bd3275f8a41cefbe24e8
xinali <daitaomail@gmail.com>
2023-12-26 10:23:42 +0800

    添加综合执行脚本简化执行步骤

---
+++ src/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# reninja
+../tools/linux_arm64/ninja -C out
+ninja_result=$?
+echo "ninja result: $ninja_result"
+
+# continue or not
+if [ $ninja_result -ne 0 ]; then
+    echo "ninja build faild with error $ninja_result"
+    exit 1
+fi
+
+
+# get test name
+test_pattern=$1
+
+# raw exec
+command="out/cc_learning_test --gtest_filter=CCLearningTest.TestFactoryPattern"
+
+# replace with new command
+exec_command=$(echo $command | sed "s/TestFactoryPattern/$test_pattern/")
+
+echo "exec: $exec_command"
+$exec_command
